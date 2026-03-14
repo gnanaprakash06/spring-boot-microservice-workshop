f70160b5d455e37d4a61bb181f230d6eaebced58
gnanaprakash <gnanaprakash653@gmail.com>
2026-03-14 13:28:36 +0530

    Initial Commit

---
+++ movie-info-service/src/test/java/dev/gnanaprakash/movie_info_service/MovieInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gnanaprakash.movie_info_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

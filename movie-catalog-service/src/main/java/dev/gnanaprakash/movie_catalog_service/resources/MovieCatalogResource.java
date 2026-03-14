7bd370d58f6875218edfa973171ea99e76e1c0aa
gnanaprakash <gnanaprakash653@gmail.com>
2026-03-14 13:47:46 +0530

    Added Movie Catalog Service API

---
+++ movie-catalog-service/src/main/java/dev/gnanaprakash/movie_catalog_service/resources/MovieCatalogResource.java
@@ -0,0 +1,22 @@
+package dev.gnanaprakash.movie_catalog_service.resources;
+
+import dev.gnanaprakash.movie_catalog_service.models.CatalogItem;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+
+    @GetMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+        return Collections.singletonList(
+                new CatalogItem("Transformers", "Test", 4)
+        );
+    }
+}
